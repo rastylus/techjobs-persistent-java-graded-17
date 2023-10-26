4f324d31a7c576c00981d2e9196670e57c03fe10
rastylus <ryan.p.riggs@gmail.com>
2023-10-26 04:57:23 -0500

    added employer and skill repository

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(max= 150)
+    private String location;
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 300)
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
